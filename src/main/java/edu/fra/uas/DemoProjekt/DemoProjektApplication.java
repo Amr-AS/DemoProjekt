6b625b65b3fd61b577589e556607863808892d7f
Amr-AS <mornixus@gmail.com>
2025-10-22 22:10:32 +0200

    first commit

---
+++ src/main/java/edu/fra/uas/DemoProjekt/DemoProjektApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.DemoProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/fra/uas/DemoProjekt/DemoProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.DemoProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
